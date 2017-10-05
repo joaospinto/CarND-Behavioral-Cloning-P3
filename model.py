91ad1a12a760b204b0a97b657caa23ceeb043061
Joao Sousa-Pinto <joao@baidu.com>
2017-10-05 13:41:17 -0700

    Adding TensorBoard and ModelCheckpoint callbacks.

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ os.environ["KERAS_BACKEND"] = "tensorflow"
 import numpy as np
 import tensorflow as tf
 
-from keras.applications.inception_v3 import InceptionV3
+from keras.callbacks import ModelCheckpoint, TensorBoard
 from keras.models import Sequential
 from keras.layers.core import Dense, Dropout, Flatten, Lambda
 from keras.layers.convolutional import Conv2D, Cropping2D
@@ -26,9 +26,15 @@ tf.app.flags.DEFINE_string(
     "directory containing driving_log.csv and IMG/*.jpg"
 )
 
+tf.app.flags.DEFINE_string(
+    'weights_file',
+    'weights.h5',
+    "file where model weights should be saved"
+)
+
 tf.app.flags.DEFINE_float(
     'correction',
-    0.015,
+    0.025, # previously 0.015
     "correction to be applied to center-steering on left and right images"
 ) #TODO: calibrate further
 
@@ -44,6 +50,18 @@ tf.app.flags.DEFINE_integer(
     "number of training epochs"
 )
 
+tf.app.flags.DEFINE_integer(
+    'raw_batch_size',
+    64,
+    "batches are constructed from this many base pictures (pre-augmentation)"
+) # with flipping + side views, augmented b.s. is 6 * FLAGS.raw_batch_size
+
+tf.app.flags.DEFINE_bool(
+    'reset',
+    True,
+    "If False, previous weights will be loaded at the start."
+)
+
 def get_data():
     print("Loading data in directory {}...".format(FLAGS.data_dir))
     data = []
@@ -54,11 +72,11 @@ def get_data():
             data.append(row)
     return data
 
-def generator(data, raw_batch_size=10):
+def generator(data):
     while True:
         random.shuffle(data)
-        for first in range(0, len(data), raw_batch_size):
-            last = min(first + raw_batch_size, len(data))
+        for first in range(0, len(data), FLAGS.raw_batch_size):
+            last = min(first + FLAGS.raw_batch_size, len(data))
             center_images = []
             left_images = []
             right_images = []
@@ -113,7 +131,7 @@ def nvidia_net():
     # https://arxiv.org/pdf/1604.07316.pdf
     model = Sequential()
     model.add(Cropping2D(
-        cropping=((60, 30), (0, 0)),
+        cropping=((60, 30), (0, 0)), # lectures suggest ((50,20),(0,0))
         input_shape=(160, 320, 3)
     )) # shape (70,320,3)
     model.add(Lambda(lambda img: tf.image.resize_images(img, (35, 160))))
@@ -167,6 +185,7 @@ def get_model():
     #TODO
     # 1: write test generator that uses only a few pictures
     # 2: augmentation
+    # 3: save model after each epoch (using ModelCheckpoint callback)
     model = Sequential()
     model.add(Cropping2D(
         cropping=((60, 30), (0, 0)),
@@ -190,12 +209,11 @@ def get_model():
         padding="same"
     ))
     model.add(AveragePooling2D())
-    #model.add(Dropout(0.2))
+    model.add(Dropout(0.2))
     model.add(Flatten())
     model.add(Dense(32, activation='relu'))
     model.add(Dense(32, activation='relu'))
     model.add(Dense(32, activation='relu'))
-    #model.add(Dropout(0.2))
     model.add(Dense(1))
     return model
 
@@ -204,22 +222,28 @@ def main(_):
     random.shuffle(data)
     training_data = data[:int(0.8*len(data))]
     validation_data = data[int(0.8*len(data)):]
-    raw_batch_size = 10 # with flipping + side views, augmented b.s. is 60
-    train_gen = generator(training_data, raw_batch_size)
-    valid_gen = generator(validation_data, raw_batch_size)
+    train_gen = generator(training_data)
+    valid_gen = generator(validation_data)
     model = get_model()
     print(model.summary())
+    if not FLAGS.reset:
+        model.load_weights(FLAGS.weights_file)
     model.compile(loss='mse', optimizer='adam')
     model.fit_generator(
         generator=train_gen,
-        steps_per_epoch=int(np.ceil(len(training_data)/raw_batch_size)),
+        steps_per_epoch=int(np.ceil(len(training_data)/FLAGS.raw_batch_size)),
         epochs=FLAGS.epochs,
-        callbacks=[], #TODO
+        callbacks=[
+            ModelCheckpoint(FLAGS.weights_file, save_weights_only=True),
+            TensorBoard(
+                write_images=True,
+                batch_size=FLAGS.raw_batch_size
+            )
+        ],
+        verbose=1,
         validation_data=valid_gen,
-        validation_steps=int(np.ceil(len(validation_data)/raw_batch_size)),
-        verbose=1
+        validation_steps=int(np.ceil(len(validation_data)/FLAGS.raw_batch_size))
     )
-    model.save_weights('weights.h5', overwrite=True)
 
 if __name__ == '__main__':
     tf.app.run()
